35b32c71223288e8076f48ed3b49608f8e57cfa6
Douglas Marcelino <douglas_champion@hotmail.com>
2020-10-07 20:17:32 -0300

    Add test to generateLocationMessage function

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,15 +1,28 @@
 //npm i expect mocha --save-dev
-let expect = require('expect');
+let expect = require('expect')
 
-let {generateMessage} = require('./message');
+let {generateMessage, generateLocationMessage} = require('./message')
 
 describe('Generate Message', () => {
     it("should generate correct message object", () => {
-        let from = "Doug",
+        const from = "Doug",
             text = "Some random text",
-            message = generateMessage(from, text);
+            message = generateMessage(from, text)
+
+        expect(typeof message.createdAt).toBe('number')
+        expect(message).toMatchObject({from, text})
+    })
+})
+
+describe('Generate Location Message', () => {
+    it('should generate correct location object', () => {
+        const from = "Willian",
+            lat = 15,
+            lng = 56,
+            url = `https://www.google.com/maps?q=${lat}, ${lng}`,
+            message = generateLocationMessage(from, lat, lng)
 
         expect(typeof message.createdAt).toBe('number');
-        expect(message).toMatchObject({from, text})
+        expect(message).toMatchObject({from, url})
     })
 })
