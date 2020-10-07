97ee51852c0c7581ecc6f8415560c963a172dd9f
Douglas Marcelino <douglas_champion@hotmail.com>
2020-10-07 20:50:19 -0300

    Update util/message.js with moment lib

--- server/utils/message.js
+++ server/utils/message.js
@@ -1,8 +1,10 @@
+const moment = require('moment')
+
 let generateMessage = (from, text) => {
     return {
         from,
         text,
-        createdAt: new Date().getTime()
+        createdAt: moment().valueOf()
     }
 }
 
@@ -10,7 +12,7 @@ let generateLocationMessage = (from, lat, lng) => {
     return {
         from,
         url: `https://www.google.com/maps?q=${lat}, ${lng}`,
-        createdAt: new Date().getTime()
+        createdAt: moment().valueOf()
     }
 }
 
